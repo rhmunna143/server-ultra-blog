5dbe3b8c4352cce971a4889c4521070e6a730aef
rhmunna143 <rhmunna19@gmail.com>
2024-05-20 22:20:20 +0600

    cors update

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000', "http://localhost:5000"],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
