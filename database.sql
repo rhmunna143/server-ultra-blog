0d9e6961d26f25f2374e1011cdaa01238d5181f1
rhmunna143 <rhmunna19@gmail.com>
2024-05-20 10:34:37 +0600

    sql update

--- database.sql
+++ database.sql
@@ -1,9 +1,7 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    name VARCHAR(255),
-    profile_picture TEXT
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE posts (
