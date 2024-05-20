baae46d4a9abf1481e749125a97f79ef3f7076bb
rhmunna143 <rhmunna19@gmail.com>
2024-05-20 13:16:28 +0600

    vercel

--- db.js
+++ db.js
@@ -3,8 +3,12 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL
+// });
+
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.POSTGRES_URL,
 });
 
 console.log("Connected to the database");
